34992d4b442874f828badc5ef08a38c0f4293e7f
johnkirch123 <john.kirch.business@gmail.com>
2022-04-16 07:48:53 -0400

    google oauth changes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const passport = require("passport");
+const keys = require("./config/keys");
 
 const GoogleStrategy = require("passport-google-oauth20");
 
@@ -8,14 +9,14 @@ const app = express();
 passport.use(
   new GoogleStrategy(
     {
-      clientID: process.env[keys.GOOGLE_CLIENT_ID],
-      clientSecret: process.env[keys.GOOGLE_CLIENT_SECRET],
-      callbackURL: "/google/auth/callback",
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
     },
     (accessToken, refreshToken, profile, done) => {
       console.log(`accessToken ${accessToken}`);
       console.log(`refreshToken ${refreshToken}`);
-      console.log(`profile ${profile}`);
+      console.log(`profile ${JSON.stringify(profile)}`);
     }
   )
 );
