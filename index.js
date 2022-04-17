ca13e58235a999a82be65d4d1158db999573af6e
johnkirch123 <john.kirch.business@gmail.com>
2022-04-17 14:03:22 -0400

    authentication complete

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
-const keys = require("./config/keys");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+require("./models/User");
 require("./services/passport");
 
 mongoose.connect(keys.MongoURI, () => {
@@ -8,6 +11,15 @@ mongoose.connect(keys.MongoURI, () => {
 });
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 
--- models/User.js
+++ models/User.js
@@ -1,2 +1,11 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  googleId: {
+    type: String,
+    required: true,
+  },
+});
+
+mongoose.model("users", UserSchema);
