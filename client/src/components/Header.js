d87d7076372d4eecd6110813c0cb9f4b21cea6fa
John Kirch <john.kirch.business@gmail.com>
2022-04-19 09:20:31 -0400

    login out functionality working with redirects

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,42 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href='/auth/google'>Login With Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href='/api/logout'>Logout</a>
+          </li>
+        );
+    }
+  }
+
   render() {
-    return <div>Header</div>;
+    return (
+      <nav>
+        <div className='nav-wrapper'>
+          <a href='#' className='left brand-logo'>
+            Emaily
+          </a>
+          <ul className='right'>{this.renderContent()}</ul>
+        </div>
+      </nav>
+    );
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
