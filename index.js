22d6e678d98800bbaa5117c80639e062dc21d5d3
John Kirch <john.kirch.business@gmail.com>
2022-04-20 13:11:55 -0400

    changes for heroku deployment

--- index.js
+++ index.js
@@ -40,5 +40,6 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
+// PORT environment variable or 5000 for development
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
