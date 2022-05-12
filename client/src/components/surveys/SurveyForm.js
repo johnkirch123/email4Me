f4990fce2e8afaf470571b813c86307f17eedafc
johnkirch123 <john.kirch.business@gmail.com>
2022-05-12 16:18:42 -0400

    redux survey

---
+++ client/src/components/surveys/SurveyForm.js
@@ -0,0 +1,65 @@
+import _ from "lodash";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { reduxForm, Field } from "redux-form";
+import SurveyField from "./SurveyField";
+
+const FIELDS = [
+  { label: "Survey Title", name: "title" },
+  { label: "Subject Line", name: "subject" },
+  { label: "Email Body", name: "body" },
+  { label: "Recipient List", name: "emails" },
+];
+
+class SurveyForm extends Component {
+  renderFields() {
+    return _.map(FIELDS, ({ label, name }) => {
+      return (
+        <Field
+          key={label}
+          component={SurveyField}
+          type="text"
+          label={label}
+          name={name}
+        />
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <form
+          onSubmit={this.props.handleSubmit((values) => console.log(values))}
+        >
+          {this.renderFields()}
+          <Link
+            to="/dashboard"
+            className="red btn-flat white-text"
+            type="submit"
+          >
+            Cancel
+          </Link>
+          <button className="teal btn-flat right white-text" type="submit">
+            Next <i className="material-icons right">done</i>
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+function validate(values) {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = "Please provide a title";
+  }
+
+  return errors;
+}
+
+export default reduxForm({
+  validate,
+  form: "surveyForm",
+})(SurveyForm);
