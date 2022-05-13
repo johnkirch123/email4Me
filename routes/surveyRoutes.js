16489bc333544a070044676cafdd9f7f1d88c5bf
johnkirch123 <john.kirch.business@gmail.com>
2022-05-13 13:01:21 -0400

    heroku webhook testing

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -37,4 +37,9 @@ module.exports = (app) => {
       res.status(422).send(err);
     }
   });
+
+  app.post("/api/surveys/webhooks", (req, res) => {
+    console.log(req.body);
+    res.send({});
+  });
 };
